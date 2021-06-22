8f0e57d512192e2dc0e77210db060f00a4029319
Sergio Pomárico <sergiodavid21@gmail.com>
2021-06-22 08:09:35 -0500

    add types to navigations

--- src/navigation/auth.tsx
+++ src/navigation/auth.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import OnboardingScreen from '../screens/onboarding';
+import WelcomeScreen from '../screens/welcome';
+import {Route} from '../core/types';
 
-const AuthStack = createStackNavigator();
+const AuthStack = createStackNavigator<Route>();
 
 const AuthStackScreen = () => (
   <AuthStack.Navigator headerMode="none">
     <AuthStack.Screen name="Onboarding" component={OnboardingScreen} />
+    <AuthStack.Screen name="Welcome" component={WelcomeScreen} />
   </AuthStack.Navigator>
 );
 
