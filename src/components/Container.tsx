c8dba5d60fc37428fb3256207bc88591332ed08e
Sergio Pomárico <sergiodavid21@gmail.com>
2021-07-04 19:00:42 -0500

    add keyboard scrolling behavior on forms

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,8 @@ import React, {ReactNode} from 'react';
 
 import {useTheme} from '@config/theme';
 import {Box} from '@components';
-import {Image, Dimensions, StyleSheet, StatusBar, Platform} from 'react-native';
+import {Image, Dimensions, StyleSheet, Platform} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 interface ContainerProps {
   children: ReactNode;
@@ -11,7 +12,7 @@ interface ContainerProps {
 
 const isIOS = Platform.OS === 'ios';
 
-const {width} = Dimensions.get('screen');
+const {width, height: sHeight} = Dimensions.get('screen');
 
 const aspectRatio = 750 / 1125;
 const height = width * aspectRatio;
@@ -21,48 +22,49 @@ export const patterns = [require('../assets/login-pattern.png')];
 const Container = ({children, footer}: ContainerProps) => {
   const theme = useTheme();
   return (
-    <Box flex={1} backgroundColor="secondary">
-      <StatusBar barStyle="light-content" />
-      <Box backgroundColor="white">
-        <Box
-          borderBottomLeftRadius="xl"
-          overflow="hidden"
-          height={height * 0.65}>
+    <KeyboardAwareScrollView scrollEnabled={false}>
+      <Box height={sHeight} backgroundColor="secondary">
+        <Box backgroundColor="white">
+          <Box
+            borderBottomLeftRadius="xl"
+            overflow="hidden"
+            height={height * 0.65}>
+            <Image
+              source={patterns[0]}
+              style={{
+                width,
+                height,
+                borderBottomLeftRadius: theme.borderRadii.xl,
+              }}
+            />
+          </Box>
+        </Box>
+        <Box flex={1} overflow="hidden">
           <Image
             source={patterns[0]}
             style={{
               width,
               height,
-              borderBottomLeftRadius: theme.borderRadii.xl,
+              ...(StyleSheet.absoluteFill as object),
+              top: -height * 0.65,
             }}
           />
+          <Box
+            flex={1}
+            backgroundColor="white"
+            borderRadius="xl"
+            borderTopLeftRadius={0}>
+            {children}
+          </Box>
         </Box>
-      </Box>
-      <Box flex={1} overflow="hidden">
-        <Image
-          source={patterns[0]}
-          style={{
-            width,
-            height,
-            ...(StyleSheet.absoluteFill as object),
-            top: -height * 0.65,
-          }}
-        />
         <Box
-          flex={1}
-          backgroundColor="white"
-          borderRadius="xl"
-          borderTopLeftRadius={0}>
-          {children}
+          backgroundColor="secondary"
+          paddingBottom={isIOS ? 'l' : 'm'}
+          paddingTop="l">
+          {footer}
         </Box>
       </Box>
-      <Box
-        backgroundColor="secondary"
-        paddingBottom={isIOS ? 'l' : 'm'}
-        paddingTop="l">
-        {footer}
-      </Box>
-    </Box>
+    </KeyboardAwareScrollView>
   );
 };
 
