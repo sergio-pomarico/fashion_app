5b635e4d8068f62aabe72e1f1d59b4d25ba5e9e2
Sergio Pomárico <sergiodavid21@gmail.com>
2021-07-11 10:12:33 -0500

    add color property to link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 import {Text} from '@components';
 import {TouchableOpacity} from 'react-native';
+import {Theme} from '@config/theme';
 
 interface LinkProps {
   label: string;
+  color?: keyof Theme['colors'];
   onPress: () => void;
 }
 
-const Link = ({label, onPress}: LinkProps) => {
+const Link = ({label, color, onPress}: LinkProps) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <Text variant="body" color="primary" textDecorationLine="underline">
+      <Text variant="body" color={color || 'primary'}>
         {label}
       </Text>
     </TouchableOpacity>
