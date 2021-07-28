79719436e0c2e2c8e98b109c3a474253eb2365a6
Sergio Pomárico <sergiodavid21@gmail.com>
2021-07-27 19:31:42 -0500

    masonry layout

---
+++ src/screens/favoritesOutfits/components/Outfit.tsx
@@ -0,0 +1,27 @@
+import {Box} from '@components';
+import React from 'react';
+
+interface OutfitProps {
+  outfit: {
+    color: string;
+    aspectRatio: number;
+    id: number;
+  };
+  width: number;
+}
+
+const Outfit = ({
+  outfit: {color: backgroundColor, aspectRatio},
+  width,
+}: OutfitProps) => {
+  console.log(aspectRatio * width);
+  return (
+    <Box
+      borderRadius="m"
+      marginBottom="m"
+      style={{backgroundColor, height: aspectRatio * width, width}}
+    />
+  );
+};
+
+export default Outfit;
--- src/screens/favoritesOutfits/index.tsx
+++ src/screens/favoritesOutfits/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {Box, Header} from '@components';
 import {AppRoutes, StackNavigationProps} from '@core/types';
 import {DrawerActions} from '@react-navigation/native';
+import {ScrollView, Dimensions} from 'react-native';
+import {outfits} from '@core/content';
+
+import Footer from './components/Footer';
+import Outfit from './components/Outfit';
+import {useTheme} from '@config/theme';
+
+const {width: wWidth} = Dimensions.get('screen');
 
 const FavoritesOutfitScreen = ({
   navigation,
 }: StackNavigationProps<AppRoutes, 'FavoritesOutfits'>) => {
+  const theme = useTheme();
+  const [footerHeight, setFooterHeight] = useState(0);
+  const width = (wWidth - theme.spacing.m * 3) / 2;
   return (
     <Box flex={1} backgroundColor="white">
       <Header
@@ -17,6 +28,42 @@ const FavoritesOutfitScreen = ({
         }}
         right={{icon: 'shopping-bag', onPress: () => {}}}
       />
+      <Box flex={1}>
+        <ScrollView
+          contentContainerStyle={{
+            padding: theme.spacing.m,
+            paddingBottom: footerHeight,
+          }}>
+          <Box flexDirection="row">
+            <Box marginRight="m">
+              {outfits
+                .filter((_, i) => i % 2 !== 0)
+                .map(outfit => (
+                  <Outfit outfit={outfit} {...{width}} key={outfit.id} />
+                ))}
+            </Box>
+            <Box>
+              {outfits
+                .filter((_, i) => i % 2 === 0)
+                .map(outfit => (
+                  <Outfit outfit={outfit} {...{width}} key={outfit.id} />
+                ))}
+            </Box>
+          </Box>
+        </ScrollView>
+      </Box>
+      <Box
+        position="absolute"
+        bottom={0}
+        left={0}
+        right={0}
+        onLayout={({
+          nativeEvent: {
+            layout: {height},
+          },
+        }) => setFooterHeight(height)}>
+        <Footer onPress={() => {}} label="Add to favorites" />
+      </Box>
     </Box>
   );
 };
