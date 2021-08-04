7c4dba7e143de9f5950bb5279a0fc76ed14f3888
Sergio Pomárico <sergiodavid21@gmail.com>
2021-08-03 22:18:34 -0500

    card bunce animation

--- src/screens/outfitIdeas/components/Card.tsx
+++ src/screens/outfitIdeas/components/Card.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Dimensions, StyleSheet} from 'react-native';
-import {PanGestureHandler} from 'react-native-gesture-handler';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
 import Animated, {
   interpolateColor,
   useSharedValue,
   useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring,
 } from 'react-native-reanimated';
 
 import {Box} from '@components';
@@ -12,6 +17,11 @@ import {Box} from '@components';
 interface CardProps {
   position: number;
 }
+
+type Context = {
+  offsetX: number;
+  offsetY: number;
+};
 
 const {width: wWidth} = Dimensions.get('screen');
 const width = wWidth * 0.75;
@@ -21,39 +31,80 @@ const borderRadius = 24;
 const mix = (value: number, x: number, y: number): number =>
   x * (1 - value) + y * value;
 
+export const snapPoint = (
+  value: number,
+  velocity: number,
+  points: ReadonlyArray<number>,
+): number => {
+  'worklet';
+  const point = value + 0.2 * velocity;
+  const deltas = points.map(p => Math.abs(point - p));
+  const minDelta = Math.min.apply(null, deltas);
+  return points.filter(p => Math.abs(point - p) === minDelta)[0];
+};
+
+const α = Math.PI / 12;
+const A = Math.sin(α) * height + Math.cos(α) * width;
+const snapPoints = [-A, 0, A];
+
 const Card = ({position}: CardProps) => {
-  const pos = useSharedValue(position);
-  const translateX = useSharedValue(0);
   const backgroundColor = interpolateColor(
-    pos.value,
+    position,
     [0, 1],
     ['#C9E9E7', '#74BCb8'],
   );
-  const translateY = mix(pos.value, 0, -50);
-  const scale = mix(pos.value, 1, 0.9);
+  const translateOffsetY = mix(position, 0, -50);
+  const scale = mix(position, 1, 0.9);
+
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(translateOffsetY);
+
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    Context
+  >({
+    onStart: (_, ctx) => {
+      ctx.offsetX = translateX.value;
+      ctx.offsetY = translateY.value;
+    },
+    onActive: ({translationX, translationY}, ctx) => {
+      translateX.value = ctx.offsetX + translationX;
+      translateY.value = ctx.offsetY + translationY;
+    },
+    onEnd: ({velocityX, velocityY}) => {
+      const dest = snapPoint(translateX.value, velocityX, snapPoints);
+      translateX.value = withSpring(dest, {velocity: velocityX});
+      translateY.value = withSpring(0, {velocity: velocityY});
+    },
+  });
 
   const rStyle = useAnimatedStyle(() => {
     return {
-      backgroundColor,
-      borderRadius,
-      height,
-      width,
       transform: [
-        {
-          translateX: translateX.value,
-        },
+        {translateY: translateY.value},
+        {translateX: translateX.value},
         {scale},
-        {translateY},
       ],
     };
   });
+
   return (
     <Box
       style={StyleSheet.absoluteFillObject}
       justifyContent="center"
       alignItems="center">
-      <PanGestureHandler>
-        <Animated.View style={rStyle} />
+      <PanGestureHandler {...{onGestureEvent}}>
+        <Animated.View
+          style={[
+            {
+              backgroundColor,
+              borderRadius,
+              height,
+              width,
+            },
+            rStyle,
+          ]}
+        />
       </PanGestureHandler>
     </Box>
   );
